839fc8b96d1cc46e7edeb28e184e514952dda7c4
Vasil Mkrtchyan <m-v-g@mail.ru>
2022-07-02 16:50:23 +0400

    data parsing

--- VS Code/GITC/das_22/reg_exp.cpp
+++ VS Code/GITC/das_22/reg_exp.cpp
@@ -2,6 +2,8 @@
 #include <regex>
 using namespace std;
 
+
+
 int main()
 {
     regex hamaranisher("([0-9]{2}[A-Z]{2}[0-9]{3})|([0-9]{3}[A-Z]{2}[0-9]{2})"); //haykakan hamaranisheri shablon
@@ -9,9 +11,15 @@ int main()
     regex heraxos("(\\+374|0)[0-9]{2} [0-9]{2}(-| )?[0-9]{2}(-| )?[0-9]{2}");
     regex anun("[A-Z]{1}[a-z]{3,} [A-Z]{1}[a-z]{3,}(ian|yan|iants|yants|uni)");
     //regex date("[0-31]{1}(\\.|\\-|/| ){1}[0-12]{1}(\\.|\\-|/){1}[0-9]{4} [0-24]{1}:[0-59]{1}(:[0-59]{1})");
-    regex date("[0-3]{1}[0-9]{1}(\\.|\\-|/| )[0-1]{1}[0-9]{1}(\\.|\\-|/| )[0-9]{4} [0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}(:[0-5]{1}[0-9]{1})?");
+    //regex date("[0-3]{1}[0-9]{1}(\\.|\\-|/| )[0-1]{1}[0-9]{1}(\\.|\\-|/| )[0-9]{4} [0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}(:[0-5]{1}[0-9]{1})?");
+    regex date ("((0[0-9])|(1[0-9])|(2[0-9])|(3[01]))(\\.|\\-|/| )"
+                "((0[0-9])|(1[012]))(\\.|\\-|/| )"
+                "((19[7-9][0-9])|(20[0-9][0-9])) "
+                "((0[0-9])|(1[0-9])|(2[0-4])):"
+                "([0-5]{1}[0-9]{1})"
+                "(:([0-5]{1}[0-9]{1}))?");
     regex pasport("[A-Z]{2}[0-9]{7}");
-    regex ipv4("");
+   
 
     string text = "gfgfh 45AV111 jhjh 222AV45"
     "news.am 467646457oiyiuuih News.am"
@@ -21,6 +29,8 @@ int main()
 
 
     smatch vxojdenie; //klas vor@ kpahi sparich informacia
+    
+    
 
     while(regex_search(text, vxojdenie, hamaranisher))
     {
@@ -52,5 +62,7 @@ int main()
         text = vxojdenie.suffix().str();
     }
 
+
     return 0;
 }
+
